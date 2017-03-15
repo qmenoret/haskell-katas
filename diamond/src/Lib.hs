b02b54385eb0a76e12862bb96fc5bc72e02a4291
qmenoret <menoret.quentin@gmail.com>
2017-03-15 21:03:47 +0100

    Improve solution

--- diamond/src/Lib.hs
+++ diamond/src/Lib.hs
@@ -7,18 +7,21 @@ import Data.Char
 diamond :: Char -> String
 diamond 'A' = "A"
 diamond 'a' = "A"
-diamond z = total
+diamond c = total
     where
-        total = first ++ '\n':lines ++ first
+        total = init lines
         lines = (unlines flines) ++ (unlines llines)
-        flines = buildLines xs (length xs - 1) 1
+        flines = buildLines chars (length chars - 1) (-1)
         llines = reverse (init flines)
-        first = (replicate (length xs) ' ') ++ "A" ++ (replicate (length xs) ' ')
-        (c:xs) = ['A'..(toUpper z)]
-         
+        chars = ['A'..(toUpper c)]
+
 buildLines :: String -> Int -> Int -> [String]
 buildLines "" _ _ = []
 buildLines (c:cs) b i = line c b i :(buildLines cs (b - 1) (i + 2))
 
 line :: Char -> Int -> Int -> String
-line c spaceB spaceI = (replicate spaceB ' ') ++ [c] ++ (replicate spaceI ' ') ++ [c] ++ (replicate spaceB ' ')
+line c spaceB spaceI = spaces ++ (center spaceI) ++ spaces
+    where
+        spaces = (replicate spaceB ' ')
+        center (-1) = [c]
+        center i = [c] ++ (replicate i ' ') ++ [c]
