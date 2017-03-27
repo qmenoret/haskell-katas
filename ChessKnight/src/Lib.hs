e1c51df06418a631acaf94c70d25ed134a681840
qmenoret <menoret.quentin@gmail.com>
2017-03-27 13:48:02 +0200

    Improve ChessKnight using do notation

--- ChessKnight/src/Lib.hs
+++ ChessKnight/src/Lib.hs
@@ -1,6 +1,7 @@
 module Lib
     where
 
+import Control.Monad
 import Data.List.Unique
 
 newtype Knight = Knight {knightPos :: (Int, Int)}
@@ -10,14 +11,24 @@ moves :: [Knight] -> Int -> [Knight]
 moves ks 0 = ks
 moves ks n = sortUniq $ moves (ks >>= moveKnights) (n-1)
 
+-- moveKnights :: Knight -> [Knight]
+-- moveKnights (Knight (x, y)) = filter (\ (Knight (a,b)) -> a >= 0 && b >= 0 && a < 8 && b < 8)
+--                                 [ Knight (x -1, y -2)
+--                                 , Knight (x -1, y +2)
+--                                 , Knight (x +1, y -2)
+--                                 , Knight (x +1, y +2)
+--                                 , Knight (x -2, y -1)
+--                                 , Knight (x -2, y +1)
+--                                 , Knight (x +2, y -1)
+--                                 , Knight (x +2, y +1)
+--                                 ]
+
 moveKnights :: Knight -> [Knight]
-moveKnights (Knight (x, y)) = filter (\ (Knight (a,b)) -> a >= 0 && b >= 0 && a < 8 && b < 8)
-                                [ Knight (x -1, y -2)
-                                , Knight (x -1, y +2)
-                                , Knight (x +1, y -2)
-                                , Knight (x +1, y +2)
-                                , Knight (x -2, y -1)
-                                , Knight (x -2, y +1)
-                                , Knight (x +2, y -1)
-                                , Knight (x +2, y +1)
-                                ]
+moveKnights (Knight (x, y)) = do
+    (x', y') <- [ (x -1, y -2), (x -1, y +2)
+                , (x +1, y -2), (x +1, y +2)
+                , (x -2, y -1), (x -2, y +1)
+                , (x +2, y -1), (x +2, y +1)
+                ]
+    guard (x' >= 0 && y' >= 0 && x' < 8 && y' < 8)
+    return (Knight (x', y'))
